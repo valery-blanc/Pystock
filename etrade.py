2babcd6c0bf3d601fa515c37b5da7ce860446a3d
Valery Blanc <vblanc@gmail.com>
2021-10-20 22:14:51 +0200

    wrappers

--- etrade.py
+++ etrade.py
@@ -5,7 +5,7 @@ consumer_key = "7054284dfb42fd505030408d4ec0d4d3"
 consumer_secret = "1e9dee8bc1bd2b62a3a555560f635bbf4689512f03441e1bdea0a0487f79a108"
 
 
-def gettokens():
+def get_tokens():
     oauth = pyetrade.ETradeOAuth(consumer_key, consumer_secret)
     print(oauth.get_request_token())  # Use the printed URL
 
@@ -18,13 +18,27 @@ def list_accounts(tokens):
     return accounts.list_accounts()
 
 def get_quote(symbols, tokens):
-    maket = pyetrade.ETradeMarket(consumer_key,consumer_secret,tokens['oauth_token'],tokens['oauth_token_secret'],dev=False)
-    quote = maket.get_quote(symbols, detail_flag='intraday', skip_mini_options_check=True, resp_format='json')
+    market = pyetrade.ETradeMarket(consumer_key,consumer_secret,tokens['oauth_token'],tokens['oauth_token_secret'],dev=False)
+    quote = market.get_quote(symbols, detail_flag='intraday', skip_mini_options_check=True, resp_format='json')
     #print (quote)
     return quote
     #for q in quote['QuoteResponse']['QuoteData']:
     #    print(q['All'])
 
+def get_last_price (market, symbol):
+    start = time.time()
+    quote = market.get_quote([symbol], detail_flag='intraday', skip_mini_options_check=True, resp_format='json')
+    last_price = quote['QuoteResponse']['QuoteData'][0]['Intraday']['lastTrade']
+    end = time.time()
+    print(f'get_last_price : {end - start}')
+    return last_price
+
+def get_market(tokens):
+    start = time.time()
+    market = pyetrade.ETradeMarket(consumer_key,consumer_secret,tokens['oauth_token'],tokens['oauth_token_secret'],dev=False)
+    end = time.time()
+    print(f'get_market : {end - start}')
+    return market
 
 def stream_quote(symbol,tokens):
     start = time.time()
@@ -79,20 +93,33 @@ def list_order (tokens, account_id_key):
     print(f"list : {orders.list_orders(account_id_key, resp_format='json')}")
 
 
-tokens = {'oauth_token': 'hQRjEPLUdeOM0Xj0GHhv8V8z1zanULHtaj5H5rZ50FM=', 'oauth_token_secret': 'YhhTnyVkvT35MtX4HL/Ah1N5CbC5O8BK/sknx5CzW2s='}
+#get_tokens()
+#quit()
+
+
+tokens = {'oauth_token': 'n1ToLbNbZD3noo9NYSvDbzDdNZzzzHwGrfGEOECf2SM=', 'oauth_token_secret': 'JiU+Pk9PtJ58Cg1/DWVxN+9m8Q+OCY+/obVm/4+Y/q8='}
 symbol = 'ZNGA'
 
-accounts = list_accounts(tokens)
-print (f'accounts:{accounts}')
-account_id_key = accounts['AccountListResponse']['Accounts']['Account']['accountIdKey']
-print (f'account_id_key:{account_id_key}')
+market = get_market(tokens)
+for i in range(0,20):
+    nyc_datetime = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime("%Y-%m-%d %H:%M:%S.%f")
+    last = get_last_price(market, symbol)
+    print (f'{nyc_datetime} {symbol} {last}')
 
-#list_order(tokens, account_id_key)
-nyc_datetime = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime("%Y-%m-%d %H:%M:%S.%f")
-quote = get_quote([symbol], tokens)
-q = quote['QuoteResponse']['QuoteData'][0]['Intraday']
-print(f" {nyc_datetime} lastTrade : {q['lastTrade']}")
-#myorder = preview_and_place (symbol, tokens, account_id_key)
+
+
+
+#accounts = list_accounts(tokens)
+#print (f'accounts:{accounts}')
+#account_id_key = accounts['AccountListResponse']['Accounts']['Account']['accountIdKey']
+#print (f'account_id_key:{account_id_key}')
+#
+##list_order(tokens, account_id_key)
 #nyc_datetime = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime("%Y-%m-%d %H:%M:%S.%f")
-#print(f"{nyc_datetime}  myorder : {myorder}")
-list_order (tokens, account_id_key)
+#quote = get_quote([symbol], tokens)
+#q = quote['QuoteResponse']['QuoteData'][0]['Intraday']
+#print(f" {nyc_datetime} lastTrade : {q['lastTrade']}")
+##myorder = preview_and_place (symbol, tokens, account_id_key)
+##nyc_datetime = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime("%Y-%m-%d %H:%M:%S.%f")
+##print(f"{nyc_datetime}  myorder : {myorder}")
+#list_order (tokens, account_id_key)
